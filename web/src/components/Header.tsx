eb4f0d24e628beebf1b551ef1bad556338275c14
Amanda Fernandes <aferanda@gmail.com>
2022-06-06 11:03:49 -0300

    style: change name and unit of measurement

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,12 +1,12 @@
 import styles from './Header.module.css';
 
-import rocket from '../assets/rocket.svg';
+import rocketImg from '../assets/rocket.svg';
 
 export function Header() {
   return (
     <header className={styles.header}>
       <div>
-        <img src={rocket} alt="rocket" />
+        <img src={rocketImg} alt="rocket" />
         <h1>
           to
           <span>do</span>
