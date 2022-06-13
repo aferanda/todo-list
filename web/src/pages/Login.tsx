a4f724d36b405ee8eda5e0daa4f6d8bb474e99b7
Amanda Fernandes <aferanda@gmail.com>
2022-06-13 17:23:56 -0300

    feat: create login screen

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,11 +1,57 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { Header } from "../components/Header";
+import { createUser } from "../service/api";
+
+import styles from './Login.module.css';
 
 export function Login() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({ username: "", email: "" });
+
+  function handleChangeInput(event: ChangeEvent<HTMLInputElement>) {
+    const { target: { name, value } } = event;
+    setUser(state => ({ ...state, [name]: value }));
+  }
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+    const userId = await createUser(user);
+    setUser({ username: "", email: "" });
+    navigate('/tasks', { state: userId });
+  }
+
   return (
     <>
       <Header />
-      <section>
-        <h1>login</h1>
+      <section className={styles.loginBox}>
+        <div className={styles.loginText}>
+          <h2>seu gerenciador de tarefas sempre a mão</h2>
+          <p>...porque você não precisa se lembrar de tudo.</p>
+        </div>
+        <form
+          className={styles.loginInfo}
+          onSubmit={handleLogin}
+        >
+          <input
+            type="text"
+            name="username"
+            value={user.username}
+            placeholder="Name"
+            onChange={handleChangeInput}
+          />
+          <input
+            type="email"
+            name="email"
+            value={user.email}
+            placeholder="Email"
+            onChange={handleChangeInput}
+          />
+          <button>
+            Entrar
+          </button>
+        </form>
       </section>
     </>
   )
