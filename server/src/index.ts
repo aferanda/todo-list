0163e34b213e4288abaa681098b18047fe8a95cc
Amanda Fernandes <aferanda@gmail.com>
2022-06-13 18:59:45 -0300

    chore: add port env variable

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
